7d6bcdd50f9c90c8944b4016e12c1a13022b5d93
yujeongoyj <oyj3989288@gmail.com>
2023-01-10 01:28:47 +0900

    13 commit

---
+++ 13/js/quiz-1.js
@@ -0,0 +1,9 @@
+const quotesURL = "http://dummyjson.com/quotes";
+
+fetch(quotesURL)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+   
+  })
+  .catch(error => console.log(error));
